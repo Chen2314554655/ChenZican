341917f2936bace8792bf30366071f40380f4494
Chen2314554655 <78737595+Chen2314554655@users.noreply.github.com>
2021-04-09 23:29:02 +0200

    Update dirll9.7.4.cpp

--- dirll9.7.4.cpp
+++ dirll9.7.4.cpp
@@ -1,6 +1,18 @@
 #include "std_lib_facilities.h"
 
-const vector<string> months {
+enum class Month 
+{
+     jan, feb, mar, apr, may, jun, july, aug, sept, oct, nov, dec
+};
+
+Month operator++(Month& m)
+{
+   m = (m == Month::dec) ? Month::jan : Month(int(m) + 1);
+   return m;
+}
+
+vector<string> months
+{
 	"January",
 	"February",
 	"March",
@@ -14,17 +26,6 @@ const vector<string> months {
 	"November",
 	"December"
 };
-
-enum class Month
-{ 
-    jan,feb,mar,apr,may,jun,july,aug,sept,oct,nov,dec 
-};
-
-Month operator++(Month& m)
-{
-	m = (m == Month::dec) ? Month::jan : Month(int(m)+1);
-	return m;
-}
 
 ostream& operator<<(ostream& os, Month m)
 {
