6d947d8d64a2c2f3473998f3d2bd0903f905d717
Chen2314554655 <78737595+Chen2314554655@users.noreply.github.com>
2021-04-09 21:06:36 +0200

    Add files via upload

---
+++ dirll9.7.1.cpp
@@ -0,0 +1,82 @@
+#include "std_lib_facilities.h"
+
+const vector<string> months
+{
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+};
+
+enum class Month 
+{
+     jan, feb, mar, apr, may, jun, july, aug, sept, oct, nov, dec
+};
+
+Month operator++(Month& m)
+{
+   m = (m == Month::dec) ? Month::jan : Month(int(m) + 1);
+   return m;
+}
+
+ostream& operator<<(ostream& os, Month m)
+{
+   return os << months[int(m)];
+}
+
+class Date {
+private:
+    Year y;
+    Month m;
+    int d; 
+public:
+    Date(): y(1978), m(Month::jun), d(25) {}
+
+    Date(Year yy, Month mm, int dd): y(yy), m(mm), d(dd)
+    {
+        if (d < 0 || d > 31)
+            error("invalid day");
+    }
+
+	Year get_year() const { return y; }
+	Month get_month() const { return m; }
+	int get_day() const { return d; }
+
+	void add_day(int n)
+	{
+         d += n;
+         while(d > 31)
+          {
+              ++m;
+              d -= 31;
+              if (m == Month::jan)
+            {
+                 ++y;
+            }
+          }        
+
+      };
+
+ostream& operator<<(ostream& os, const Date& date)
+{
+	return os << date.get_year() << ". "
+		<< date.get_month() << " "
+		<< date.get_day() << ".";
+}
+
+int main()
+try {
+    Date today(1978, Month::jun, 25);
+	Date tomorrow = today;
+	tomorrow.add_day(1);
+	cout << today << endl << tomorrow << endl;
+	return 0;
+}
